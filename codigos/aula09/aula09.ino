1f40bc12b514549bd2025d68061fa4b5c2ecf380
Joao Silva <30415539+diskrat@users.noreply.github.com>
2026-04-29 17:06:55 -0300

    Remove serial communication functionality
    Removed UART initialization and serial communication code.

--- codigos/aula09/aula09.ino
+++ codigos/aula09/aula09.ino
@@ -1,40 +1,10 @@
-/* Frequência do clock do microcontrolador (Hz) */
-#define FOSC 16000000
 
-/* Taxa de transmissão serial (bps) */
-#define BAUD 9600
-
-/* Valor do registrador UBRR para o baud rate definido */
-#define MYUBRR FOSC / 16 / BAUD - 1
-
-/* Inicializa a UART para transmissão e recepção serial */
-/* Configura o USART0 para 8 bits, sem paridade e com RX/TX habilitados */
-void serial_init()
-{
-  /* Configura o baud rate e os registradores de controle da UART */
-  UBRR0H = (MYUBRR >> 8);
-  UBRR0L = MYUBRR;
-  UCSR0B = (1 << RXEN0) | (1 << TXEN0);
-  UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
-}
-
-/* Envia uma string pela UART */
-/* msg: ponteiro para string terminada em nulo */
-void serial_send(const char *msg)
-{
-  for (int i = 0; msg[i] != '\0'; i++)
-  {
-    while (!(UCSR0A & (1 << UDRE0)))
-      ;
-    UDR0 = msg[i];
-  }
-}
 
 /* Variáveis globais para armazenar leitura do ADC e valor de saída */
 int sensorValue = 0;
 int outputValue = 0;
 
-/* Configura o ADC, PWM e inicializa a UART */
+/* Configura o ADC e PWM  */
 void setup()
 {
   /* Define a referência de tensão como AVcc (5V) no canal ADC0 */
@@ -51,7 +21,6 @@ void setup()
   /* Define PB1 como saída digital para o LED */
   DDRB |= (1 << PB1);
 
-  serial_init();
 }
 
 /* Faz leitura do ADC, define PWM e envia a saída via serial */
@@ -67,11 +36,4 @@ void loop()
   /* Mapeia o valor lido para a faixa 0-255 e ajusta o PWM */
   outputValue = map(sensorValue, 0, 1023, 0, 255);
   OCR1A = outputValue;
-
-  /* Formata e envia a saída via serial */
-  char msg_buf[64];
-  int percent = outputValue * 100 / 255;
-  snprintf(msg_buf, sizeof(msg_buf), "ADC = %d | %% Motor = %d\n", sensorValue, percent);
-  serial_send(msg_buf);
-  delay(50);
 }
